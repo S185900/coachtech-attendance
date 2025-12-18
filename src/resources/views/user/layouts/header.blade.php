e1274eb5b1bff8f16c8c230cecd9f8e21ae43dd5
S <serena.sa.em.na.ra@gmail.com>
2025-12-18 13:00:21 +0900

    ステージングされたビューファイルをコミット

---
+++ src/resources/views/user/layouts/header.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coachtech-attendance</title>
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/header.css')}}">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    @yield('css')
+</head>
+<!-- ログイン前共通ヘッダー http://localhost/login -->
+<body>
+    <header class="header">
+        <a href="/" class="header-logo">
+            <img class="header-logo-img" src="{{ asset('images/coachtech-logo.png') }}" alt="COACHTECH">
+        </a>
+    </header>
+    <main class="main-content">
+        @yield('content')
+    </main>
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,11 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('admin/login', function () {
+
+
+// Route::get('/register', fn () => view('auth.register'))->name('register');
+// Route::post('/register', [RegisteredUserController::class, 'store']);
+
+// Route::get('/login', fn () => view('auth.login'))->name('login');
+// Route::post('/login', [LoginUserController::class, 'store']);
+
+
+Route::get('/register', function () {
     return view('auth.admin.login');
 });
 
-Route::get('/', function () {
+Route::get('/login', function () {
+    return view('user.auth.login');
+});
+
+Route::get('/attendance', function () {
+    return view('user.auth.login');
+});
+
+Route::get('/admin/login', function () {
+    return view('admin.auth.login');
+});
+
+Route::get('/admin/attendance/list', function () {
     return view('auth.register');
 });
 
