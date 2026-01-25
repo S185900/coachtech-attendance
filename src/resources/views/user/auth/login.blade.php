9bce2dfff1fb58a0a988a5df682f6da396c444cf
S <serena.sa.em.na.ra@gmail.com>
2026-01-25 20:18:39 +0900

    ユーザーのログイン・登録・認証メールページのクリーンアップ

--- src/resources/views/user/auth/login.blade.php
+++ src/resources/views/user/auth/login.blade.php
@@ -1,46 +1,47 @@
 @extends('user.layouts.header')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/login.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/login.css')}}">
 @endsection
 
 <!-- 一般ユーザーログイン画面 http://localhost/login-->
 @section('content')
-<h1 class="section-title">ログイン</h1>
-<section class="login-section">
-    <form action="{{ route('login') }}" method="POST" novalidate>
-        @csrf
+    <h1 class="section-title">ログイン</h1>
 
-        <div class="login-item">
-            <label for="email" class="login-label">メールアドレス</label>
-            <input id="email" type="email" class="input-form" name="email" value="{{ old('email') }}" required autocomplete="email">
+    <section class="login-section">
+        <form action="{{ route('login') }}" method="POST" novalidate>
+            @csrf
 
-            @error('email')
-                <span class="form-error-message">{{ $message }}</span>
-            @enderror
-        </div>
+            <div class="login-item">
+                <label for="email" class="login-label">メールアドレス</label>
+                <input id="email" type="email" class="input-form" name="email" value="{{ old('email') }}" required autocomplete="email">
 
-        <div class="login-item">
-            <label for="password" class="login-label">パスワード</label>
-            <input id="password" type="password" class="input-form" name="password" required autocomplete="password">
+                @error('email')
+                    <span class="form-error-message">{{ $message }}</span>
+                @enderror
+            </div>
 
-            @error('password')
-                <span class="form-error-message">{{ $message }}</span>
-            @enderror
-            @error('auth_error')
-                <span class="auth-error-message">{{ $message }}</span>
-            @enderror
-        </div>
+            <div class="login-item">
+                <label for="password" class="login-label">パスワード</label>
+                <input id="password" type="password" class="input-form" name="password" required autocomplete="password">
 
-        <div class="login-item">
-            <button class="login-button" type="submit">
-                ログインする
-            </button>
-        </div>
-    </form>
+                @error('password')
+                    <span class="form-error-message">{{ $message }}</span>
+                @enderror
+                @error('auth_error')
+                    <span class="auth-error-message">{{ $message }}</span>
+                @enderror
+            </div>
 
-    <nav class="register-nav">
-        <a class="register-link" href="{{ route('register') }}">会員登録はこちら</a>
-    </nav>
-</section>
+            <div class="login-item">
+                <button class="login-button" type="submit">
+                    ログインする
+                </button>
+            </div>
+        </form>
+
+        <nav class="register-nav">
+            <a class="register-link" href="{{ route('register') }}">会員登録はこちら</a>
+        </nav>
+    </section>
 @endsection
--- src/resources/views/user/auth/register.blade.php
+++ src/resources/views/user/auth/register.blade.php
@@ -1,58 +1,59 @@
 @extends('user.layouts.header')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/register.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/register.css')}}">
 @endsection
 
 <!-- 一般ユーザー会員登録画面 http://localhost/register-->
 @section('content')
-<h1 class="section-title">会員登録</h1>
-<section class="register-section">
-    <form action="{{ route('register') }}" method="POST" novalidate>
-        @csrf
+    <h1 class="section-title">会員登録</h1>
 
-        <div class="register-item">
-            <label for="name" class="register-label">名前</label>
-            <input id="name" type="text" class="input-form" name="name" value="{{ old('name') }}" required autocomplete="name">
+    <section class="register-section">
+        <form action="{{ route('register') }}" method="POST" novalidate>
+            @csrf
 
-            @error('name')
-                <span class="form-error-message">{{ $message }}</span>
-            @enderror
-        </div>
+            <div class="register-item">
+                <label for="name" class="register-label">名前</label>
+                <input id="name" type="text" class="input-form" name="name" value="{{ old('name') }}" required autocomplete="name">
 
-        <div class="register-item">
-            <label for="email" class="register-label">メールアドレス</label>
-            <input id="email" type="email" class="input-form" name="email" value="{{ old('email') }}" required autocomplete="email">
+                @error('name')
+                    <span class="form-error-message">{{ $message }}</span>
+                @enderror
+            </div>
 
-            @error('email')
-                <span class="form-error-message">{{ $message }}</span>
-            @enderror
-        </div>
+            <div class="register-item">
+                <label for="email" class="register-label">メールアドレス</label>
+                <input id="email" type="email" class="input-form" name="email" value="{{ old('email') }}" required autocomplete="email">
 
-        <div class="register-item">
-            <label for="password" class="register-label">パスワード</label>
-            <input id="password" type="password" class="input-form" name="password" required autocomplete="new-password">
+                @error('email')
+                    <span class="form-error-message">{{ $message }}</span>
+                @enderror
+            </div>
 
-            @error('password')
-                <span class="form-error-message">{{ $message }}</span>
-            @enderror
-        </div>
+            <div class="register-item">
+                <label for="password" class="register-label">パスワード</label>
+                <input id="password" type="password" class="input-form" name="password" required autocomplete="new-password">
 
-        <div class="register-item">
-            <label for="password_confirmation" class="register-label">パスワード確認</label>
+                @error('password')
+                    <span class="form-error-message">{{ $message }}</span>
+                @enderror
+            </div>
 
-            <input id="password_confirmation" type="password" class="input-form" name="password_confirmation" required autocomplete="new-password">
-        </div>
+            <div class="register-item">
+                <label for="password_confirmation" class="register-label">パスワード確認</label>
 
-        <div class="register-item">
-            <button class="register-button" type="submit">
-                登録する
-            </button>
-        </div>
-    </form>
+                <input id="password_confirmation" type="password" class="input-form" name="password_confirmation" required autocomplete="new-password">
+            </div>
 
-    <nav class="login-nav">
-        <a class="login-link" href="{{ route('login') }}">ログインはこちら</a>
-    </nav>
-</section>
+            <div class="register-item">
+                <button class="register-button" type="submit">
+                    登録する
+                </button>
+            </div>
+        </form>
+
+        <nav class="login-nav">
+            <a class="login-link" href="{{ route('login') }}">ログインはこちら</a>
+        </nav>
+    </section>
 @endsection
